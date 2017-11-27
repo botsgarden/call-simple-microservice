6e08a3a9610bb1cb3762af9d27b08a48b53889b9
k33g <ph.charriere@gmail.com>
2017-11-27 09:53:40 +0100

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-PORT=8080 SERVICE_PORT=8080 java  -jar target/call-simple-microservice-1.0-SNAPSHOT-fat.jar
+HTTPBACKEND_HOST=localhost \
+HTTPBACKEND_PORT=9090 \
+PORT=9096 SERVICE_PORT=9096 \
+java  -jar target/call-simple-microservice-1.0-SNAPSHOT-fat.jar
